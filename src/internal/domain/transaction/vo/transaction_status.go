759f4175bd3ddc7eebb730fb9639decfc3754975
Oleg <neevidd@yandex.ru>
2024-12-17 16:00:14 +0300

    commit

--- src/internal/domain/transaction/vo/transaction_status.go
+++ src/internal/domain/transaction/vo/transaction_status.go
@@ -1,1 +1,18 @@
 package vo
+
+// TODO: vo.TransactionStatus // status: initiated, completed, failed, canceled
+
+const (
+	statusInitiated = "initiated"
+	statusCompleted = "completed"
+	statusFailed    = "failed"
+	statusCanceled  = "canceled"
+)
+
+type TransactionStatus struct {
+	Status string
+}
+
+func NewTransactionStatus() TransactionStatus {
+	return TransactionStatus{Status: statusInitiated}
+}
