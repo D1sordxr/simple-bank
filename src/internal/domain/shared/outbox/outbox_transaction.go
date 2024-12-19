040b7448eee7b1068d84e16f1e336a19fe583025
Oleg <neevidd@yandex.ru>
2024-12-20 02:39:04 +0300

    tx outbox

--- src/internal/domain/shared/outbox/outbox_transaction.go
+++ src/internal/domain/shared/outbox/outbox_transaction.go
@@ -1,11 +1,36 @@
 package outbox
 
 import (
+	"github.com/D1sordxr/simple-banking-system/internal/domain/shared/outbox/vo"
+	sharedVO "github.com/D1sordxr/simple-banking-system/internal/domain/shared/vo"
 	"github.com/D1sordxr/simple-banking-system/internal/domain/transaction"
+	"time"
 )
 
-// TODO: NewTransactionOutbox()
-
-func NewTransactionOutbox(transaction transaction.Aggregate) Outbox {
-	return Outbox{}
+func NewTransactionOutbox(tx transaction.Aggregate) (Outbox, error) {
+	outboxID := sharedVO.NewUUID()
+	aggregateID := vo.NewTransactionAggregateID()
+	aggregateType := vo.NewTransactionAggregateType()
+	messageType, err := vo.NewMessageType(vo.TypeCreated)
+	if err != nil {
+		return Outbox{}, err
+	}
+	messagePayload, err := vo.NewMessagePayload(tx)
+	if err != nil {
+		return Outbox{}, err
+	}
+	outboxStatus, err := vo.NewOutboxStatus(vo.StatusPending)
+	if err != nil {
+		return Outbox{}, err
+	}
+	creationTime := time.Now()
+	return Outbox{
+		OutboxID:       outboxID,
+		AggregateID:    aggregateID,
+		AggregateType:  aggregateType,
+		MessageType:    messageType,
+		MessagePayload: messagePayload,
+		Status:         outboxStatus,
+		CreatedAt:      creationTime,
+	}, nil
 }
