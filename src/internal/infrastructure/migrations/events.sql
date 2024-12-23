2f0f9799b47f8beb1a31ba0d361e3487b782bc53
Oleg <neevidd@yandex.ru>
2024-12-23 19:42:32 +0300

    fixes

--- src/internal/infrastructure/migrations/events.sql
+++ src/internal/infrastructure/migrations/events.sql
@@ -1,9 +1,9 @@
 CREATE TABLE events (
-    id UUID PRIMARY KEY,                -- Уникальный идентификатор события
-    aggregate_id UUID NOT NULL,         -- Идентификатор агрегата (клиента, счёта, транзакции)
-    aggregate_type VARCHAR(50) NOT NULL, -- Тип агрегата (client, account, transaction)
-    event_type VARCHAR(50) NOT NULL,    -- Тип события (created, updated, deleted и т.д.)
-    payload JSONB NOT NULL,             -- Данные события в формате JSON
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Время создания события
-    version INT NOT NULL                -- Версия события для обеспечения последовательности
+    id UUID PRIMARY KEY,
+    aggregate_id UUID NOT NULL,
+    aggregate_type VARCHAR(50) NOT NULL,
+    event_type VARCHAR(50) NOT NULL,
+    payload JSONB NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    version INT NOT NULL
 );
